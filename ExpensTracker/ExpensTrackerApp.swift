18e17a5d33393dfcdebd69b29d993530ce4950b3
Hajime Miyazaki <vsj2015.us@gmail.com>
2024-06-11 12:19:16 -0700

    Initial Commit

---
+++ ExpensTracker/ExpensTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensTrackerApp.swift
+//  ExpensTracker
+//
+//  Created by Hajime Miyazaki on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpensTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
